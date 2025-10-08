dd077d37b69339b7a235f7a03ce212d838ce8947
Utkarsh Uday <utkarshsahil629@gmail.com>
2025-10-09 04:06:24 +0530

    assignment completed

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,49 +1,57 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
-} from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
-import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import wallet from '../turbin3-wallet.json';
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
+} from '@metaplex-foundation/mpl-token-metadata';
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from '@metaplex-foundation/umi';
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey('HrbX2x56CPEUAQdJAp4abhSZS6nTWLCr6Ctj9g6kKpbX');
 
 // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi('https://api.devnet.solana.com', 'confirmed');
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
-umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
+umi.use(signerIdentity(signer));
 
-(async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+try {
+  // Start here
+  let accounts: CreateMetadataAccountV3InstructionAccounts = {
+    mint,
+    mintAuthority: signer,
+  };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+  let data: DataV2Args = {
+    name: 'Saitama OPM',
+    symbol: 'OPM',
+    uri: 'https://raw.githubusercontent.com/solana-turbin3/Q4_25_Builder_utkarshuday/main/solana-starter/assets/saitama.json',
+    sellerFeeBasisPoints: 500, // 5%
+    creators: null,
+    collection: null,
+    uses: null,
+  };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+  let args: CreateMetadataAccountV3InstructionArgs = {
+    data,
+    isMutable: true,
+    collectionDetails: null,
+  };
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+  let tx = createMetadataAccountV3(umi, {
+    ...accounts,
+    ...args,
+  });
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
-})();
+  let result = await tx.sendAndConfirm(umi);
+  console.log(bs58.encode(result.signature));
+} catch (e) {
+  console.error(`Oops, something went wrong: ${e}`);
+}
--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,28 +1,73 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
-import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from '@solana/web3.js';
+import wallet from '../turbin3-wallet.json';
+import {
+  getOrCreateAssociatedTokenAccount,
+  TOKEN_PROGRAM_ID,
+  transfer,
+  transferChecked,
+} from '@solana/spl-token';
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
-const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const commitment: Commitment = 'confirmed';
+const connection = new Connection('https://api.devnet.solana.com', commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
-
+const mint = new PublicKey('HrbX2x56CPEUAQdJAp4abhSZS6nTWLCr6Ctj9g6kKpbX');
+const decimals = 6;
+const token_decimals = 10n ** BigInt(decimals);
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const from = new PublicKey('BpG1zEBLGfpdoAodsQcpsuCwhnBmQRbPa6UaLmVWfZh5');
 
-(async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+const to = new PublicKey('HwrjaPLqsq3YuR6cuK93oNtpGSsXoUtQ4oY9GwuYf2Vy');
+const transferAmount = 50n * token_decimals;
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+try {
+  // Get the token account of the fromWallet address, and if it does not exist, create it
+  const fromWallet = await getOrCreateAssociatedTokenAccount(
+    connection,
+    keypair,
+    mint,
+    from,
+    undefined,
+    commitment,
+    undefined,
+    TOKEN_PROGRAM_ID
+  );
+  // Get the token account of the toWallet address, and if it does not exist, create it
+  const toWallet = await getOrCreateAssociatedTokenAccount(
+    connection,
+    keypair,
+    mint,
+    to,
+    undefined,
+    commitment,
+    undefined,
+    TOKEN_PROGRAM_ID
+  );
+  // Transfer the new token to the "toTokenAccount" we just created
+  const txSignature = await transferChecked(
+    connection,
+    keypair,
+    fromWallet.address,
+    mint,
+    toWallet.address,
+    keypair,
+    transferAmount,
+    decimals
+  );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
-})();
+  console.log(
+    `Success! Check out your TX here: https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+  );
+} catch (e) {
+  console.error(`Oops, something went wrong: ${e}`);
+}
